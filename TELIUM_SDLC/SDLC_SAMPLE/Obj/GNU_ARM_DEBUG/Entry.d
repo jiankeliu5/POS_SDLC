965cca2a7e27fda1538b662e3cfac74afbdd326a
Henrique Hornos <henrique.hornos@gmail.com>
2013-07-20 10:19:51 -0300

    inclusao projeto sdlc telium - inacabado para testar commits de inclusao e modificacao de linhas de codigo

---
+++ TELIUM_SDLC/SDLC_SAMPLE/Obj/GNU_ARM_DEBUG/Entry.d
@@ -0,0 +1,211 @@
+Obj/GNU_ARM_DEBUG/Entry.o: Src/Entry.c \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/SDK30.H \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/ta_appli.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/clib.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/loaderapi.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/oem.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/basearm.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/xecarm.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/oem_public.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/rtctime.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/trace_sap.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/uiram.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/gprs_lib.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/noy_rtc_public.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/fs.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/ssl_profile.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/nttypes.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/_emvdctag_.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/iso2.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/appel.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/etat.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/config_TManager.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/config.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/diagnostic.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/diag_def.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/defdiag_TSys.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/defdiag_TManager.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/reloc.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/typ.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/csymbol.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/oem_display.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/libgr.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/fontlib.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/police.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/entry.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/saisie.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/MSGlib.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/saisie_ppad.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/oemfrance.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/modem.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/fct_gen.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/ccext.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/socle.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/services.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/status.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/param.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/_emvdc_.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/emvseq.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/os.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/pfpsai.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/pinpad.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/util_sq.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/lib_iapp.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/oem_hid.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/oem_com.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/oem_sysfioctl.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/callhost.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/psc.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/message.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/ManagerMessages.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/msgmgr.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/flashmanager.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/caisse.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/protocole.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/schmgmt.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/submitpin.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/oem_power.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/oem_vfs.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/oem_mmc.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/dbluetooth.h \
+  C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/P30.h
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/SDK30.H:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/ta_appli.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/clib.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/loaderapi.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/oem.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/basearm.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/xecarm.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/oem_public.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/rtctime.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/trace_sap.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/uiram.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/gprs_lib.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/noy_rtc_public.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/fs.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/ssl_profile.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/nttypes.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/_emvdctag_.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/iso2.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/appel.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/etat.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/config_TManager.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/config.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/diagnostic.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/diag_def.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/defdiag_TSys.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/defdiag_TManager.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/reloc.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/typ.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/csymbol.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/oem_display.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/libgr.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/fontlib.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/police.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/entry.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/saisie.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/MSGlib.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/saisie_ppad.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/oemfrance.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/modem.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/fct_gen.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/ccext.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/socle.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/services.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/status.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/param.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/_emvdc_.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/emvseq.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/os.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/pfpsai.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/pinpad.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/util_sq.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/lib_iapp.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/oem_hid.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/oem_com.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/oem_sysfioctl.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/callhost.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/psc.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/message.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/ManagerMessages.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/msgmgr.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/flashmanager.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/caisse.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/protocole.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/schmgmt.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/submitpin.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/oem_power.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/oem_vfs.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/oem_mmc.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/dbluetooth.h:
+
+C:/Arquivos\ de\ programas/SDK30/SDK\ 7.5.1/SDK/inc/P30.h:
---
+++ TELIUM_SDLC/SDLC_SAMPLE/Src/porting/inc/porting.h
@@ -0,0 +1,90 @@
+/******************************************************************************
+ * Modulo Fonte..........: porting.h                                          *
+ *                                                                            *
+ * Data de criacao.......: 12/07/2013                                         *
+ * Desenvolvedor.........: Henrique Hornos                                    *
+ *                                                                            *
+ *----------------------------------------------------------------------------*
+ * Descricao.............:                                                    *
+ * prototipos de funcoes do modulo app_sys.h                                  *
+ *----------------------------------------------------------------------------*
+ * Alteracoes............:                                                    *
+ * Desenvolvedor.........:                                                    *
+ * Data da alteracao.....:                                                    *
+ *                                                                            *
+ * "porting.h, v0.0.1 2013/07/11 20:45:00 id: "Henrique_H1"                   *
+ *                                                                            *
+ ******************************************************************************/
+#ifndef PORTING_H
+#define PORTING_H
+#endif
+
+/* defines de status do modem SDLC */
+#define		SDLC_RESETED		0x0100
+#define		SDLC_DIALLING		0x0200
+#define		SDLC_CONNECTED		0x0300
+#define		SDLC_ERRO_MDM		0x0400
+#define		SDLC_NO_CARRIER		0x0500
+#define		SDLC_NO_DIALTONE	0x0600
+#define		SDLC_BUSY_DETECT	0x0700
+#define		SDLC_NO_ANSWER		0x0800
+#define		SDLC_ATCMD_TMO		0x0900
+
+#define		AT_INIT_SDLC		 0				/* indice inicial de programacao do modem SDLC */
+#define		AT_FIN_SDLC			 6				/* indice final de programacao do modem SDLC */
+#define		AT_DISCA_SDLC		 7				/* indice do comando AT de discagem - necessario edita-lo anteriormente */
+#define		AT_CMD_RESP			-1				/* obtem somente resposta do modem */
+
+#define		TMO_DISC_SDLC		60000l			/* 60 segundos */
+#define 	COM_TIMEOUT			20
+
+#define		DTR_ON				0x01
+#define		DTR_OFF				0x00
+#define		RTS_ON				0x02
+#define		RTS_OFF				0x00
+#define		DTR_RTS_ON			0x03
+#define		CTS_ON				0x20
+#define		DCD_ON				0x08
+
+/* defines BEEP POS */
+#define		BEEP_DEFAULT	0x01
+#define		BEEP_ERRO		0x02
+
+/* estrutura de parametros SDLC */
+struct PARM_SDLC
+{
+	uchar	atenuacao;							/* atenuacao da portadora (0-15) */
+	BOOL	pulso;								/* modo de discagem */
+	char	pabx[12];							/* string de PABX */
+	uchar	ndial;								/* # de dials consecutivos */
+	char	fone[12];							/* fone a ser discado */
+	uchar	ntry;								/* # de tentativas por fone */
+	uchar	tmo_idle;							/* time out para idle */
+};
+
+void	debugH			(char *, ...);
+
+STAT	resetConsole	( void );
+STAT	resetPrint		( void );
+
+STAT	beepPOS			( char );
+STAT	get_parm		( const char *, char *, int );
+STAT	waitTime		( ulong );
+
+int		writeDisp		( char *, int, int, int );
+void	clearDisplay	( void );
+char	readKeyPos		( void );
+ulong	get_milli_sec	( void );
+
+/* SDLC */
+STAT	resetSDLC		( void );
+STAT	SaveConfSDLC	( struct PARM * );
+STAT	discaSDLC		( void );
+STAT	chk_sdlc		( void );
+STAT	getStatSDLC		( void );
+STAT	sndSDLC			( char *, int );
+STAT	recvSDLC		( char *, int * );
+
+STAT	ATCmdSDLC		( int, int );
+
+void	flushMDM		( void );
